84f2b8abf9de434595ea9bd2abf036c5de26607c
Aleksandr Boynichenko <AlexBoynich@github.com>
2023-01-26 19:39:31 +0700

    tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,40 +1,37 @@
 import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
-const outputPlain = readFileSync('__fixtures__/correct_output', 'utf8');
-const outputTree = readFileSync('__fixtures__/correct_output_complex', 'utf8');
-const outputList = readFileSync('__fixtures__/correct_output_plain', 'utf8');
+const outputPlain = readFileSync('__fixtures__/correct_output.txt', 'utf8');
+const outputTree = readFileSync('__fixtures__/correct_output_complex.txt', 'utf8');
+const outputList = readFileSync('__fixtures__/correct_output_plain.yml', 'utf8');
 const outputJson = readFileSync('__fixtures__/correct_output_json.json', 'utf8');
 
-describe('stringify format', () => {
-  const formatType = 'stylish';
   test('gendiff for .json', () => {
     const path1 = 'file1.json';
     const path2 = 'file2.json';
-    expect(genDiff(path1, path2, formatType)).toEqual(outputPlain);
+    expect(genDiff(path1, path2, 'stylish')).toEqual(outputPlain);
   });
   test('gendiff for .yml', () => {
     const path1 = 'file1.yml';
     const path2 = 'file2.yml';
-    expect(genDiff(path1, path2, formatType)).toEqual(outputPlain);
+    expect(genDiff(path1, path2)).toEqual(outputPlain);
   });
   test('gendiff for tree .json', () => {
-    const path1 = 'file1.tree.json';
-    const path2 = 'file2.tree.json';
-    expect(genDiff(path1, path2, formatType)).toEqual(outputTree);
+    const path1 = 'file1complex.json';
+    const path2 = 'file2complex.json';
+    expect(genDiff(path1, path2, 'stylish')).toEqual(outputTree);
   });
-});
 
 test('gendiff for tree .json, plain format', () => {
   const path1 = 'file1.tree.json';
   const path2 = 'file2.tree.json';
   const formatType = 'plain';
-  expect(genDiff(path1, path2, formatType)).toEqual(outputList);
+  expect(genDiff(path1, path2)).toEqual(outputList);
 });
 
 test('gendiff for tree .json, json format', () => {
   const path1 = 'file1.tree.json';
   const path2 = 'file2.tree.json';
   const formatType = 'json';
-  expect(genDiff(path1, path2, formatType)).toEqual(outputJson);
+  expect(genDiff(path1, path2)).toEqual(outputJson);
 });
