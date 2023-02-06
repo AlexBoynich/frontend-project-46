62215ea889298fdf04ca632003e058400dab15b7
Aleksandr Boynichenko <AlexBoynich@github.com>
2023-02-06 14:36:04 +0700

    add changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 import gendiff from '../index.js'
 import { Command } from 'commander';
+
+
 const program = new Command();
 program
   .version('0.8.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-     console.log(gendiff(filepath1, filepath2, program.opts().format));
-  })
+  .action((filepath1, filepath2, options) => {
+    console.log(findDiff(filepath1, filepath2, options.format));
+  });
 program.parse();
 program.name(gendiff());
