f187f4705ef226ee6ab8238486cd07cac37747cf
Aleksandr Boynichenko <AlexBoynich@github.com>
2023-02-07 15:57:14 +0700

    aasd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,9 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
+  .name('gendiff')
   .version('0.8.0')
   .description('Compares two configuration files and shows a difference.')
-  .name('gendiff')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
