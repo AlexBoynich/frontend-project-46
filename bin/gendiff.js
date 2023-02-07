51cf7e03ea7aa1336c98e716f797e95d30fc0504
Aleksandr Boynichenko <AlexBoynich@github.com>
2023-02-07 15:44:38 +0700

    tryeing to add .name

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,10 +7,10 @@ const program = new Command();
 program
   .version('0.8.0')
   .description('Compares two configuration files and shows a difference.')
+  .name('gendiff')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
     console.log(findDiff(filepath1, filepath2, options.format));
   });
 program.parse();
-program.name(gendiff());
