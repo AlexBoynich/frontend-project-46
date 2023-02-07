0b3303f23a1003ee74d3bc46c66c0523e7bc6443
Aleksandr Boynichenko <AlexBoynich@github.com>
2023-02-07 15:48:03 +0700

    trying to add .name

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,6 +11,6 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    console.log(findDiff(filepath1, filepath2, options.format));
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 program.parse();
